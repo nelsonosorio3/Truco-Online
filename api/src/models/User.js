76f15ab04ed753a7e9b6683a3eacb6345738defd
Guillermo Ojeda <guiojeda@gmail.com>
2021-09-14 12:29:56 -0300

    Actualizado modelo Users. Creado modelo Message.

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -2,11 +2,28 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('user', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+    // defino el modelo
+    sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        gamesPlayed: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        gamesWon: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        gamesLost: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+
+    });
 };
