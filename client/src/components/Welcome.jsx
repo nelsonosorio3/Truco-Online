a469f6b5c713d9cb4c483707c4dd37727e957e72
Fede García <garcia.fed3rico@gmail.com>
2021-09-14 11:12:11 -0300

    modificacion de boton

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import styles from './styles/Welcome.module.css'
+import styles from './styles/Welcome.module.css';
 
-export default function Welcome () {
+export default function Welcome() {
 
     return(
         <div className={styles.mainDiv}>
             <form action="" className={styles.form}>
                 <div className={styles.divButtons}>
-                    <button className={styles.btnLogIn}>Log in</button>
-                    <button className={styles.btnSignUp}>Sign up</button>
-                    <button className={styles.btnGuest}>Login as guest</button>
+                    <button className={styles.btnLogIn}>Log In</button>
+                    <Link 
+                        to='/singUp'
+                        className={styles.btnSignUp}>
+                        Sign Up
+                    </Link>
+                    <button className={styles.btnGuest}>Login as Guest</button>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
