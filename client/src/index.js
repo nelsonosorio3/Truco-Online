49b1a694c1d0a778d9461ebdc681458e08afaf72
Fede García <garcia.fed3rico@gmail.com>
2021-09-14 16:16:44 -0300

    monte el store

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
+import store from './Redux/store/index';
 import App from './App';
 
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
