fc4588ad442fa821b055cd44cdddd3dff8a14e47
nelson <nelsonsorio@hotmail.com>
2021-09-14 10:58:48 -0500

    divide routes for easier pararell work

---
+++ api/src/routes/friends.js
@@ -0,0 +1,12 @@
+const { Router }  = require("express")
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const router = Router();
+
+//todas las rutas /api/friend
+router.get('/' , (req , res) => {
+  res.json({msg:'ruta /api/friends'})
+})
+
+module.exports = router;
