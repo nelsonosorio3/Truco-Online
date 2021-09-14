44d24a45c1b3e42014d624b2cc970251e57cbfdb
Guillermo Ojeda <57275465+guillermoojeda@users.noreply.github.com>
2021-09-14 18:49:55 -0300

    Update index.js

--- client/src/Redux/actions-types/index.js
+++ client/src/Redux/actions-types/index.js
@@ -5,7 +5,7 @@ export default function signUpUser(data) {
   return function(dispatch) {
     return axios.post(`http://localhost:3001/users`, data)
       .then(response => {
-        dispatch({ type: SING_UP, payload: response.data });
+        dispatch({ type: SIGN_UP, payload: response.data });
       })
       .catch(error => console.log(error));
   };
