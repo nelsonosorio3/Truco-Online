b30a8d65ece2b03a28fdbdfdf6e50a25070d553e
Guillermo Ojeda <guiojeda@gmail.com>
2021-09-14 12:32:50 -0300

    Mejorado modelo User. Creado modelo Message.

---
+++ api/src/models/Message.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('message', {
+        sender_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        conversation_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    });
+};
