06a03471b3d99a58e8b757afee4f61afaeb97e19
nelson <nelsonsorio@hotmail.com>
2021-09-14 23:10:27 -0500

    get api/user and get user/id sketch

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -6,8 +6,16 @@ const Op = Sequelize.Op;
 const router = Router();
 
 //todas las rutas /api/user
-router.get('/' , (req , res) => {
-  res.json({msg:'ruta /api/user'});
+router.get('/' , async(req , res) => {
+  // const users = await User.findAll();
+  // res.json(users)
 });
+
+router.get("/:id", async(req, res)=>{
+  // const {id} = req.params;
+  // const user = await User.findByP(parseInt(id));
+  // if(!user) return res.sendStatus(404);
+  // res.jsaon(user);
+})
 
 module.exports = router;
