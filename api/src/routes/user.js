0cee17a0d44a9d6eea768d1b52e2a9891291026b
Guillermo Ojeda <guiojeda@gmail.com>
2021-09-15 18:18:34 -0300

    Agregada funcionalidad de POST y GET de usuarios. Agregada otras rutas de backend.

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -8,17 +8,31 @@ const router = Router();
 
 //todas las rutas /api/user
 router.get('/', async (req, res) => {
-  // const users = await User.findAll();
-  // res.json(users)
-  // res.sendStatus(404);
-  res.status(400).json({ message: "Conexión OK." })
+  try {
+    const users = await User.findAll();
+    res.json(users)
+    res.sendStatus(404);
+    res.status(400).json({ message: "Conexión OK." })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(404).send(error);
+  }
+
 });
 
 router.get("/:id", async (req, res) => {
-  // const {id} = req.params;
-  // const user = await User.findByPk(parseInt(id));
-  // if(!user) return res.sendStatus(404);
-  // res.json(user);
+
+  console.log("entro1");
+  var { id } = req.params;
+  id = parseInt(id);
+  var user = await User.findAll({
+    attributes: { exclude: 'password' },
+    where: {
+      id: id
+    }
+  });
+  if (!user) return res.sendStatus(404);
+  res.json(user);
   res.sendStatus(404);
 });
 
@@ -62,20 +76,21 @@ router.get("/:id/friend_requests_sent", async (req, res) => {
 
 router.post("/", async (req, res) => {
 
-  var { username, email } = req.body;
+  var { username, email, password } = req.body;
 
   try {
     var newUser = await User.create({
       username,
       email,
+      password,
       gamesPlayed: 0,
       gamesWon: 0,
       gamesLost: 0
     })
-    res.status(404).json({ message: "Usuario creado con éxito" });
+    res.status(200).json({ message: "Usuario creado con éxito" });
   } catch (error) {
     console.log(error)
-    res.send(error);
+    res.sendStatus(404).send(error);
   }
 })
 
