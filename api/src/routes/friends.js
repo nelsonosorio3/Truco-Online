51414b06258d90416b69152c38a7a9ce20469b17
Tomas Macchi <muma.sanmartin2011@gmail.com>
2021-09-16 11:01:19 -0300

    cambio TypeError -> Error

--- api/src/routes/friends.js
+++ api/src/routes/friends.js
@@ -18,14 +18,14 @@ router.post('/:id/:email' , (req , res) => {
 
   User.findByPk(id)
   .then(sender => {
-    if(!sender) throw new TypeError("No se encontro el id del usuario")
+    if(!sender) throw new Error("No se encontro el id del usuario")
     User.findOne({where: {email: email}})
     .then(requested => {
-      if(!requested) throw new TypeError("No se encontro el Email")
+      if(!requested) throw new Error("No se encontro el Email")
       return sender.hasUserRequested(requested)
     })
     .then((r) => {
-     if(r) throw new TypeError("Ya se ha enviado una solicitud a este usuario")
+     if(r) throw new Error("Ya se ha enviado una solicitud a este usuario")
      return User.findOne({where: {email: email}})
     })
     .then(requested2 => {
@@ -47,15 +47,15 @@ router.post('/:id/:email' , (req , res) => {
 router.put('/:id/:email' , (req , res) => {
   const {id, email} = req.params
   const {response} = req.query
-  
+
   if(!id || !email) return res.status(404).json({Error: "Missing parameters!"})
 
   User.findByPk(id)
   .then(requested => {
-    if(!requested) throw new TypeError("No se encontro el Email")
+    if(!requested) throw new Error("No se encontro el Email")
     User.findOne({where: {email: email}})
     .then(sender => {
-      if(!sender) throw new TypeError("No se encontro el id del usuario")
+      if(!sender) throw new Error("No se encontro el id del usuario")
       //Revisar teoria
       requested.Friends = {
         status: response
