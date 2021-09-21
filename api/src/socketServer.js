59c5b50b212121c16f9670005d659bff11b82b69
nelson <nelsonsorio@hotmail.com>
2021-09-21 11:49:01 -0500

    added basic game logic, betslist, choose card

--- api/src/socketServer.js
+++ api/src/socketServer.js
@@ -26,6 +26,113 @@ server.listen(9000, function () {
 
 var activeRooms = []
 
+
+const table = {
+    envidoList: [], // lista con la acumulacion de envidos
+    trucoValue: {truco: 2, retruco: 3, valeCuatro: 4}, //lista valor de trucos
+    envidoValue: {envido: 2, realEnvido: 3},  //lista valor envido individual
+    scoreToWin: 15, // puntaje para ganar partida/chico 
+    matchesToWin: 1, // partidas para ganar el juego
+    flor: true, //opcion para activar o desactivar flor
+    cumulativeScore: 1, //puntaje
+    turn: 1, // turno actual
+    betsList: {firstTurn: ["truco", "envido1", "realEnvido", "faltaEnvido", "ir al mazo"],
+                secondTurn: ["truco", "ir al mazo"],
+                thirdTurn: ["truco", "ir al mazo"],
+                firstTurnFlor: ["truco", "envido1", "realEnvido", "faltaEnvido", "ir al mazo", "flor"],
+                flor: ["con flor me achico", "con flor quiero", "contraFlorAlResto", "contraFlor"],
+                contraFlorAlResto: ["con flor me achico", "con flor quiero"],
+                contraFlor: ["con flor me achico", "con flor quiero"],
+                truco: ["no quiero truco", "quiero truco", "retruco", "valeCuatro"],
+                retruco: ["no quiero retruco", "quiero retruco"],
+                valeCuatro: ["no quiero valeCuatro", "quiero valeCuatro"],
+                envido1: ["no quiero", "quiero", "envido2", "realEnvido", "faltaEnvido"],
+                envido2: ["no quiero", "quiero", "realEnvido", "faltaEnvido"],
+                realEnvido: ["noQuiero", "faltaEnvido"],
+                faltaEnvido: ["noQuiero", "quiero"]
+                }, //la lista de apuestas posibles la idea es que es un objeto con propiedades de apuestas posibles y un array con cada posible respuesta
+    bet: false, //estado para renocer si hubo o no una apuesta y parar el flujo normal del juego
+    skip: false, // auxiliar para saltar
+  };
+
+  function buildDeck(){
+    let deck = [{ id: 1, suit: 'copas', number: 1, truco: 7},
+            { id: 2, suit: 'copas', number: 2, truco: 6 },
+            { id: 3, suit: 'copas', number: 3, truco: 5},
+            { id: 4, suit: 'copas', number: 4, truco: 14 },
+            { id: 5, suit: 'copas', number: 5, truco: 13 },
+            { id: 6, suit: 'copas', number: 6, truco: 12 },
+            { id: 7, suit: 'copas', number: 7, truco: 11},
+            { id: 10, suit: 'copas', number: 10, truco: 10 },
+            { id: 11, suit: 'copas', number: 11, truco: 9 },
+            { id: 12, suit: 'copas', number: 12, truco: 8},
+            { id: 13, suit: 'bastos', number: 1, truco: 2},
+            { id: 14, suit: 'bastos', number: 2, truco: 6 },
+            { id: 15, suit: 'bastos', number: 3, truco: 5},
+            { id: 16, suit: 'bastos', number: 4, truco: 14 },
+            { id: 17, suit: 'bastos', number: 5, truco: 13 },
+            { id: 18, suit: 'bastos', number: 6, truco: 12 },
+            { id: 19, suit: 'bastos', number: 7, truco: 11},
+            { id: 22, suit: 'bastos', number: 10, truco: 10 },
+            { id: 23, suit: 'bastos', number: 11, truco: 9 },
+            { id: 24, suit: 'bastos', number: 12, truco: 8 },
+            { id: 25, suit: 'espadas', number: 1, truco: 1},
+            { id: 26, suit: 'espadas', number: 2, truco: 6,  },
+            { id: 27, suit: 'espadas', number: 3, truco: 5},
+            { id: 28, suit: 'espadas', number: 4, truco: 14 },
+            { id: 29, suit: 'espadas', number: 5, truco: 13 },
+            { id: 30, suit: 'espadas', number: 6, truco: 12 },
+            { id: 31, suit: 'espadas', number: 7, truco: 3},
+            { id: 34, suit: 'espadas', number: 10, truco: 10 },
+            { id: 35, suit: 'espadas', number: 11, truco: 9 },
+            { id: 36, suit: 'espadas', number: 12, truco: 8 },
+            { id: 37, suit: 'oros', number: 1, truco: 7},
+            { id: 38, suit: 'oros', number: 2, truco: 6 },
+            { id: 39, suit: 'oros', number: 3, truco: 5},
+            { id: 40, suit: 'oros', number: 4, truco: 14 },
+            { id: 41, suit: 'oros', number: 5, truco: 13 },
+            { id: 42, suit: 'oros', number: 6, truco: 12 },
+            { id: 43, suit: 'oros', number: 7 , truco: 4},
+            { id: 46, suit: 'oros', number: 10, truco: 10 },
+            { id: 47, suit: 'oros', number: 11, truco: 9 },
+            { id: 48, suit: 'oros', number: 12, truco: 8 }
+    ]
+    return deck
+  }
+  
+  function shuffleDeck(deck){
+    let currentIndex = deck.length;
+    let randomIndex;
+    while(currentIndex != 0){
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [deck[currentIndex], deck[randomIndex]] = [deck[randomIndex], deck[currentIndex]];
+    }
+  
+    return deck;
+  }
+  
+  function getCard(deck){
+    return deck.pop();
+  }
+
+  function getHands(deck){
+    const playerAhand = [];
+    const playerBhand = [];
+    playerAhand.push(getCard(deck));
+    playerBhand.push(getCard(deck));
+    playerAhand.push(getCard(deck));
+    playerBhand.push(getCard(deck));
+    playerAhand.push(getCard(deck));
+    playerBhand.push(getCard(deck));
+    return [playerAhand, playerBhand]
+  }
+
+
+
+
 io.on('connection', function (socket) {
 
     socket.on('connected', function (name) {
@@ -50,6 +157,18 @@ io.on('connection', function (socket) {
     socket.on('bringActiveRooms', function () {
         io.emit('showActiveRooms', { activeRooms });
     });
+    socket.on("newRoundStarts", ()=>{
+        let deck = buildDeck();
+        deck = shuffleDeck(deck);
+        const [playerAhand, playerBhand] = getHands(deck);
+        socket.emit("newRoundStarts", playerAhand);
+        socket.broadcast.emit("newRoundStarts", playerBhand);
+        io.emit("bet", table.betsList.firstTurn);
+    });
+    socket.on("bet", function(betPick){
+        socket.broadcast.emit("bet", table.betsList[betPick])
+        console.log(table.betsList[betPick])
+    })
 
 
 
--- client/src/components/game.jsx
+++ client/src/components/game.jsx
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles/game.module.css';
+import socket from './socket';
 
 export default function Game() {
-    return(
+    const [player, setPlayer] = useState({
+        name: "player",
+        score: 0,
+        hand: [],
+        action: "",
+        mesa1: null,
+        mesa2: null,
+        mesa3: null,
+        rounds: [],
+        isTurn: false,
+        turnNumber: 1,
+        betOptions: [],
+      });
+
+    const newRoundStarts = async () => {
+        socket.emit('newRoundStarts')
+    }
+
+    const bet = async (e) =>{
+        socket.emit("bet", e.target.name)
+    }
+
+    const playCard = async (card) =>{
+      if(!player.mesa1) setPlayer({...player, hand: player.hand.filter(cardH=> card.id !== cardH.id), mesa1: card});
+      else if (!player.mesa2) setPlayer({...player, hand: player.hand.filter(cardH=> card.id !== cardH.id), mesa2: card});
+      else setPlayer({...player, hand: player.hand.filter(cardH=> card.id !== cardH.id), mesa3: card});
+      socket.emit("playCard", card)
+      console.log(card)
+    }
+    useEffect(()=>{
+        socket.on("newRoundStarts", (hand)=>{
+            console.log(hand)
+            setPlayer({...player, hand});
+        });
+        socket.on("bet", (betOptions)=>{
+            console.log(betOptions)
+            setPlayer({...player, betOptions})
+        })
+        return () =>{
+          socket.off('newRoundStarts');
+          socket.off("bet");
+        };
+      });
+      console.log(player)
+    return(<div>
             <div className={styles.image}> 
             </div>
+            <button onClick={newRoundStarts}>New round Start</button>
+            {player.betOptions?.map(betPick=><button onClick={bet} name={betPick} key={betPick} style = {{ padding: "30px" }}>{betPick}</button>)}<br/>
+            {player.hand?.map(card => <div onClick={()=>playCard(card)}><h2>{card.suit}</h2><h2>{card.number}</h2></div>)}
+            </div>
+            
     );
 };
