72aae598eebcd559569d32f3b0747d4f7d9e21b7
nelson <nelsonsorio@hotmail.com>
2021-09-21 13:10:41 -0500

    minor fix conflict rooms

--- client/src/components/rooms/Rooms.jsx
+++ client/src/components/rooms/Rooms.jsx
@@ -7,14 +7,11 @@ import JoinRoomForm from './JoinRoomForm';
 import RoomsList from './RoomsList';
 
 export default function Rooms() {
-<<<<<<< HEAD
   console.log("localStorage in Rooms", localStorage)
-=======
 
   const isinRoom = useSelector(store => store.roomsReducer.isInRoom);
   const roomId = useSelector(store => store.roomsReducer.roomId)
 
->>>>>>> 36ea982a917bf9cf93adf9880a80f2e19d5fac58
   return (
     <div>{console.log(isinRoom, roomId)}
       {
