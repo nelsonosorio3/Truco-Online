52f4d668f3d8f851bb9b0a1e8b3deed44319e7ec
Bob <bob@example.com>
2021-09-23 16:39:33 -0300

    ranking

--- client/src/components/Ranking.jsx
+++ client/src/components/Ranking.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import axios from 'axios';
+import React , { useEffect , useState } from 'react';
+import styles from './styles/Ranking.module.css'
 
 
 export default function Ranking() {
+    const [state, setState] = useState() 
+
+    function getRanking () {
+        axios(`http://localhost:3001/api/ranking`)
+        .then(response => {
+          setState({
+            state : response.data
+          })
+        })
+        .catch((error) => console.error(error))
+    }
+    
+    useEffect(() => {
+        getRanking()
+    }, [setState])
     
     
-    
-    
-    
+
+    console.log(state)
     return(
-            <div > 
-                Probando ranking
+            <div className={styles.fondo}> 
+                <section>
+                  <ul>
+                    
+                  </ul>
+                </section>
             </div>
     );
 };
