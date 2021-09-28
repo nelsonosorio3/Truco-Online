3bc2d96beedfe819c803f316916c123dcfa838af
Leonardo López <85114959+LGLT@users.noreply.github.com>
2021-09-27 19:50:26 -0500

    Tournaments limit and auto update

--- client/src/components/tournaments/Tournaments.jsx
+++ client/src/components/tournaments/Tournaments.jsx
@@ -6,26 +6,11 @@ import socket from '../socket';
 import NavBar from '../NavBar';
 import TournamentsForm from './TournamentsForm';
 import TournamentsList from './TournamentsList';
+import TournamentInCourse from './TournamentInCourse';
 
 export default function Tournaments(){
-    const[playersList, setPlayersList] = useState([]);
-
     const isinRoom = useSelector(store => store.tournamentsReducer.isInTournament);
-    const tournamentId = useSelector(store => store.tournamentsReducer.tournamentId)
-
-    useEffect(() => {
-        socket.on('sendTournamentData', (tournamentData) => {
-            setPlayersList(tournamentData.t.players)
-        })
-
-        return () => {socket.off()}
-    }, [playersList])
-
-    const updatePlayers = (event) => {
-        event.preventDefault();
-        socket.emit('bringTournamentData', (tournamentId));
-    }
-
+    
     return(
         <div>
             <NavBar />
@@ -33,16 +18,7 @@ export default function Tournaments(){
                 {
                     isinRoom
                     ?
-                        <div>{console.log(playersList)}
-                            <h3>TournamentId: {tournamentId}</h3>
-                            <form onSubmit={updatePlayers}>
-                                <button type='submit' className={styles.btn}>{'Update players'}</button>
-                            </form>
-                            <div>
-                                Actual players:
-                                {playersList.map(p => <h4 key={p}>{p}</h4> )}
-                            </div>
-                        </div>
+                        <TournamentInCourse />
                     :
                         <div>
                             <TournamentsForm />
