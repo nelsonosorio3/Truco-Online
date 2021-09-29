0c725842a64464e3e05290234e8cb3f2c9b3ed65
Guillermo Ojeda <guiojeda@gmail.com>
2021-09-29 16:08:38 -0300

    mejorado adminpanel

--- client/src/Redux/actions-types/adminPanelActions.js
+++ client/src/Redux/actions-types/adminPanelActions.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
-import { GET_USERS, FILTER_BY_EMAIL, FILTER_BY_NAME, FILTER_BY_ID } from '../actions/index';
 import {
+  GET_USERS, FILTER_BY_EMAIL, FILTER_BY_NAME, FILTER_BY_ID, SET_TOTAL_PAGES,
   ORDER_BY_GAMES_ASC, ORDER_BY_GAMES_DESC, ORDER_BY_WINS_ASC, ORDER_BY_WINS_DESC,
-  ORDER_BY_LOST_ASC, ORDER_BY_LOST_DESC, GO_TO_N_PAGE, SET_USERS_PER_PAGE,
-  ORDER_BY_USER_SINCE_ASC, ORDER_BY_USER_SINCE_DESC
+  ORDER_BY_LOST_ASC, ORDER_BY_LOST_DESC, GO_TO_N_PAGE, SET_SELECTED_PAGE, SET_USERS_PER_PAGE,
+  ORDER_BY_USER_SINCE_ASC, ORDER_BY_USER_SINCE_DESC, SET_DISPLAYED_ON_PAGE
 } from '../actions/index';
 
 
@@ -85,8 +85,40 @@ function sortByUserSinceDesc() {
   }
 }
 
+function setDisplayedOnPage(payload) {
+  return function (dispatch) {
+    return dispatch({ type: ORDER_BY_USER_SINCE_DESC, payload })
+  }
+}
+
+function setResultsPerPage(payload) {
+  return function (dispatch) {
+    return dispatch({ type: SET_TOTAL_PAGES, payload })
+  }
+}
+
+function goToPage(payload) {
+  return function (dispatch) {
+    return dispatch({ type: GO_TO_N_PAGE, payload })
+  }
+}
+
+function setSelectedPage(payload) {
+  return function (dispatch) {
+    return dispatch({ type: SET_SELECTED_PAGE, payload })
+  }
+}
+
+function setUsersPerPage(payload) {
+  return function (dispatch) {
+    return dispatch({ type: SET_USERS_PER_PAGE, payload })
+  }
+}
+
 export default {
   getUsers, filterByName, filterByEmail,
   sortByPlayedAsc, sortByPlayedDesc, sortByWonAsc, sortByWonDesc,
-  sortByLostAsc, sortByLostDesc, sortByUserSinceAsc, sortByUserSinceDesc
+  sortByLostAsc, sortByLostDesc, sortByUserSinceAsc, sortByUserSinceDesc,
+  setDisplayedOnPage, goToPage, setSelectedPage, setResultsPerPage,
+  setUsersPerPage
 };
