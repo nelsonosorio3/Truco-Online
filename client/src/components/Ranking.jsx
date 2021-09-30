b945db5730399baa8d26fc4b3f0cfc3af91f271e
nelson <nelsonsorio@hotmail.com>
2021-09-30 13:02:17 -0500

    minor fix tournament

--- client/src/components/Ranking.jsx
+++ client/src/components/Ranking.jsx
@@ -28,9 +28,9 @@ export default function Ranking() {
           <div className={styles.fondo}> 
            <h2 className={styles.title}> Mejores Jugadores</h2>
               <div className={styles.cups}> 
-                  <Cup username={one.username} pos='1' won={one.gamesWon} /> 
-                  <Cup username={two.username} pos='2' won={two.gamesWon} />
-                  <Cup username={three.username} pos='3' won={three.gamesWon} />
+                  <Cup username={one?.username} pos='1' won={one?.gamesWon} /> 
+                  <Cup username={two?.username} pos='2' won={two?.gamesWon} />
+                  <Cup username={three?.username} pos='3' won={three?.gamesWon} />
              </div>
             </div>
         </>
