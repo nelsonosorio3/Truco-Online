44b71e7cdc9dab5baf62f8aa9865704def4deca2
nelson <nelsonsorio@hotmail.com>
2021-09-30 21:25:45 -0500

    minor fix gameRequest

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -17,6 +17,7 @@ import Match from './Match';
 
 // nav
 import NavBar from './NavBar';
+// request de partida amigos
 import GameRequest from './GameRequest';
 
 export default function Profile(props) {
