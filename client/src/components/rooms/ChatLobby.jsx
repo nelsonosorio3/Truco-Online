4931aae5658d8efb39b95635228c9b7e6adc0712
Fed-Gar <garcia.fed3rico@gmail.com>
2021-10-06 12:22:54 -0300

    rooms mq

--- client/src/components/rooms/ChatLobby.jsx
+++ client/src/components/rooms/ChatLobby.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-unused-vars */
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import socket from '../socket';
-import styles from './styles/Chat.module.css'
+
+import styles from './styles/Chat.module.css';
 
 export default function ChatLobby ({name, roomId, typeofChat}) {
     const [msg, setMsg] = useState('');
@@ -9,30 +10,30 @@ export default function ChatLobby ({name, roomId, typeofChat}) {
 
     useEffect(() => {
         socket.emit('joinToGlobalChat', "lobby");
-    }, [])
+    }, []);
 
     useEffect(() => {
         socket.on('lobbyMessages', (data) => {
             setMsgs([...msgs, `${data.name}: ${data.msg}`]);
         })
 
-        return () => {socket.off("lobbyMessages")}
-    })
+        return () => {socket.off("lobbyMessages")};
+    });
 
     const divRef = useRef(null);
 
     useEffect(() => {
         divRef.current.scrollIntoView({behavior: 'smooth'});
-    })
+    });
 
     const submit = (event) => {
         event.preventDefault();
         socket.emit('lobbyMessage', ({name, msg, roomId}), localStorage.isAuth);
         setMsg("");
-    }
+    };
 
-    return(
-        <div>
+    return (
+        <div className={styles.contChat}>
             <div className={typeofChat==='chatLobby' ? styles.chatLobby : styles.chatGame}>
                 {msgs.map((element, i) => ( <div key={i}><div>{element}</div><div>{element.msg}</div></div> ))}
                 <div ref={divRef}></div>
@@ -42,15 +43,22 @@ export default function ChatLobby ({name, roomId, typeofChat}) {
                     typeofChat==='chatLobby'
                     ? 
                     <>
-                        <textarea placeholder={'Escribe tu mensaje...'} name="" id="" cols="95" rows="1" value={msg} onChange={event => setMsg(event.target.value)}></textarea>
+                        <textarea 
+                            placeholder={'Escribe tu mensaje...'} 
+                            name="" 
+                            id="" 
+                            cols="95" 
+                            rows="1" 
+                            value={msg} 
+                            onChange={event => setMsg(event.target.value)}>
+                        </textarea>
                         <button className={styles.btn}>Enviar</button>
                         {/* <input placeholder='Message...' type="text" id="" cols="95" rows="1" value={msg} onChange={event => setMsg(event.target.value)}></input> */}
                     </>
                     : 
                     <input type="text" id="" cols="31" rows="1" value={msg} onChange={event => setMsg(event.target.value)} className={styles.writeMessageGame}></input>
                 }
-                
             </form>
         </div>
-    )
-}
+    );
+};
--- client/src/components/rooms/RoomsList.jsx
+++ client/src/components/rooms/RoomsList.jsx
@@ -1,40 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch} from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { setIsInRoom } from '../../Redux/actions-types/roomsActions';
 import socket from '../socket';
-import styles from './styles/RoomsList.module.css'
 import GameRequest from '../GameRequest';
+
+import styles from './styles/RoomsList.module.css';
+
 export default function RoomsList(){
-    const [allRooms, setAllRooms] = useState([])
+    const [allRooms, setAllRooms] = useState([]);
     // const [roomId, setRoomId] = useState('')
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
         socket.on('showActiveRooms', (rooms) => {
             setAllRooms([rooms]);
-        })
+        });
         // socket.emit('bringActiveRooms') //automaticamente hacer update de rooms cuando hay cambios en allRooms?
 
         return () => {socket.off()}
-    }, [allRooms])
+    }, [allRooms]);
 
     useEffect(()=>{
         socket.emit('bringActiveRooms')  // traer todas las rooms disponibles al entrar
-    }, [])
+    }, []);
 
     useEffect(()=>{
         socket.on("newRoomCreated", () => {socket.emit('bringActiveRooms')}) //actualizar en tiempo real rooms disponibles
         return ()=>{
-            socket.off("newRoomCreated")
-        }
-    })
+            socket.off("newRoomCreated");
+        };
+    });
 
     const joinRoom = async (event) => {
         event.preventDefault();
-        socket.emit('joinRoom', (parseInt(event.target[0].value)), localStorage.user, localStorage.token)
-        dispatch(setIsInRoom({isInRoom: true, roomId: parseInt(event.target[0].value)}))
-      }
+        socket.emit('joinRoom', (parseInt(event.target[0].value)), localStorage.user, localStorage.token);
+        dispatch(setIsInRoom({isInRoom: true, roomId: parseInt(event.target[0].value)}));
+    };
 
     return(
         <div>
@@ -46,14 +48,18 @@ export default function RoomsList(){
                     allRooms[0].activeRooms.map(room => 
                     <div key={room.id}>
                         <form onSubmit={joinRoom}>
-                            <button type='submit' value={room.id} className={styles.roomBtn}>Room Id: {room.id} | Host: {room.host}</button>
+                            <button 
+                                type='submit' 
+                                value={room.id} 
+                                className={styles.roomBtn}>Room Id: {room.id} | Host: {room.host}
+                            </button>
                         </form>
                     </div>
                     )
                 :
-                    <></>
+                    null
                 }
             </div>
         </div>
-    )
-}
+    );
+};
